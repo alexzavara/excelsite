b617c1827436423bff7481ce1ce32ff39a0f3652
alexzavara <alex.zavara1@gmail.com>
2023-05-28 22:29:26 +0300

    exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './scss/index.scss'
-import './index.html'
+import {Excel} from './components/excel/Excel';
+import './scss/index.scss';
+
+const excel = new Excel('#app', {
+  components: []
+});
+
+console.log('Excel', excel);
