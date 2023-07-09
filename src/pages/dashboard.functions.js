76e9f016d5bcc049373561e12623dfa45a85ac3e
alexzavara <alex.zavara1@gmail.com>
2023-07-09 20:22:44 +0300

    Проверка названия таблицы

--- src/pages/dashboard.functions.js
+++ src/pages/dashboard.functions.js
@@ -2,7 +2,9 @@ function toHTML(key) {
   const thisStorage = JSON.parse(localStorage[key])
   return `
     <li class="db__record">
-      <a href="#excel/${key.slice(6)}">${thisStorage.title}</a>
+      <a href="#excel/${key.slice(6)}">
+        ${thisStorage.title ? thisStorage.title : 'Без названия'}
+      </a>
       <strong>12.06.2020</strong>
     </li>
   `
